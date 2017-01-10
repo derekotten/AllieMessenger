36e14a6747b4b3bae69dc2f97fdba5eb1878736e
Derek Otten <derek.otten@allstate.com>
2017-01-10 14:19:22 -0600

    adding ability to request recipients facebook info

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request');
 const app = express();
 const token = process.env.FB_PAGE_ACCESS_TOKEN;
 const sendMessageUrl = 'https://graph.facebook.com/v2.6/me/messages';
+const getUserInfoUrl = 'https://graph.facebook.com/v2.6/';
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -29,7 +30,7 @@ app.get('/webhook/', function (req, res) {
 });
 
 // Spin up the server
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
 });
 
@@ -44,38 +45,23 @@ app.post('/webhook/', function (req, res) {
 
             let messageText = event.message.text;
 
-
-            sendTextMessage(sender, getResponseText(messageText.substring(0, 200)));
+            sendTextMessage(sender, messageText.substring(0, 200));
         }
     }
     res.sendStatus(200)
 });
 
-
-function getResponseText(messageText) {
-
-    if (messageText.indexOf("car") != -1) {
-        return "Are you looking for help with car insurance?"
-    }
-    else if(messageText.indexOf("house") != -1) {
-        return "Are you looking for help with homeowners insurance?"
-    }else {
-        return messageText
-    }
-}
-
-
-function sendTypingAction(sender) {
+function sendTextMessage(sender, text) {
+    let messageData = {text: getResponseText(sender, text)};
     request({
         url: sendMessageUrl,
         qs: {access_token: token},
         method: 'POST',
         json: {
             recipient: {id: sender},
-            sender_action: 'typing_on'
+            message: messageData
         }
-    },
-    function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -84,21 +70,60 @@ function sendTypingAction(sender) {
     })
 }
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text };
-    request({
-        url: sendMessageUrl,
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+function getResponseText(sender, messageText) {
+
+    if (messageText.indexOf("car") != -1) {
+        return "Are you looking for help with car insurance?"
+    }
+    else if (messageText.indexOf("house") != -1) {
+        return "Are you looking for help with homeowners insurance?"
+    }
+    else if (messageText.indexOf("hello") != -1) {
+        return "Hello " + getUserDetails(sender);
+    }
+    else {
+        return messageText
+    }
 }
+
+
+function getUserDetails(sender) {
+    request({
+            url: getUserInfoUrl + sender,
+            qs: {
+                access_token: token,
+                fields: ['first_name', 'last_name']
+            },
+            method: 'GET'
+        },
+        function (error, response, body) {
+            if (error) {
+                console.log('Error getting user info: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+            else {
+                return body.first_name + ' ' + body.last_name;
+            }
+        })
+}
+
+function sendTypingAction(sender) {
+    request({
+            url: sendMessageUrl,
+            qs: {access_token: token},
+            method: 'POST',
+            json: {
+                recipient: {id: sender},
+                sender_action: 'typing_on'
+            }
+        },
+        function (error, response, body) {
+            if (error) {
+                console.log('Error sending typing action: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+        })
+}
+
